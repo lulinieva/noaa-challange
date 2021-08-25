d9dc294c48624eb903ae0a467a26d6c94f85fcd7
lulinieva <evelynieva@hotmail.com>
2021-08-25 20:01:17 -0300

    Se genera nueva repo NOAA-CHALLENGE

---
+++ src/main/java/ar/com/ada/api/noaachallenge/NoaachallengeApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.noaachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoaachallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoaachallengeApplication.class, args);
+	}
+
+}
